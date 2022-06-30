6692cb15e931d4e63f884b5be27c4a075f12afb1
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-06-30 12:12:41 -0700

    gttgtg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(void)
+{
+	printf("sizeof a Char:%d byte(s)\n", sizeof(char));
+	printf("size of an Int:%d byte(s)\n", sizeof(int));
+	printf("size of a Long int:%d byte(s)\n", sizeof(long int));
+	printf("size of a Float: %d byte(s)\n", sizeof(float));
+	printf("size of a Double:%d byte(s)\n", sizeof(double));
+	printf("size of a Long double: %d byte(s)\n", sizeof(long double));
+	return (0);
+}

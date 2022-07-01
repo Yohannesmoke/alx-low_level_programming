a4f6b72e690ac422a6e7fc02dc374722af7f851e
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-01 10:51:03 -0700

    the commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - entry point
- *
- * return: 0 after printing the function
- */
+* main -> prints exactly Programming is like building a
+* multilingual puzzle and a new line.
+* Return: 0 if excuted properly
+*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-

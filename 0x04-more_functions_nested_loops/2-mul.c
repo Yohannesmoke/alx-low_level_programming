f5604c70a209ae27fddbbc7673f25ce574556d25
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-07 04:37:48 -0700

    hkjhj

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* mul -> this is a function that will
+* @a: parameter 1
+* @b: parameter 2
+* multiplies
+* Return: returns the value of the mult
+*/
+
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+printf("%d ", result);
+return (0);
+}

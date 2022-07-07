0260c0e86979615bc3302adaf3872ac7b1380698
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-07 10:37:01 -0700

    thank you

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*more_numbers -> print number from 1 to 9
+*return:return 0 and 1
+*/
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 14; j++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}

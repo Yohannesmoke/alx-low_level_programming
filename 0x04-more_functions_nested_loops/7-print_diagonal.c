74d3a5eaeefcc3eab943272841db153333c18125
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-07 11:44:49 -0700

    seventh commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_diagonal -> print number from 1 to 9
+*@n: parameter
+*Return:return 0 and 1
+*/
+void print_diagonal(int n)
+{
+int i;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+_putchar(92);
+_putchar('\n');
+}
+}

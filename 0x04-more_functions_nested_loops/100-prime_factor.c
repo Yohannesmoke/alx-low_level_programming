91203c58d5f4416b07e31d44ffb8ccade8da48cc
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-08 13:52:10 -0700

    tr

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,36 +1,83 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 /**
-* print_triangle - Function that prints a triangle, followed by a new line.
-* @size: the int for the paramaters of my function
+* main - prints the biggest prime factor of a number.
+*
 * Return: Always 0.
 */
-void print_triangle(int size)
+int main(void)
 {
-int x, y = 0;
-int space;
+long int number;
 
-if (size > 0)
+number = 612852475143;
+
+if (isPrime(number) == 1)
 {
-while (y < size)
-{
-space = size - y - 1;
-while (x < size)
-{
-if (space > x)
-{
-_putchar(' ');
+printf("%ld\n", number);
 }
 else
 {
-_putchar('#');
+printf("%ld\n", biggestFactor(number));
 }
-x++;
+return (0);
 }
-y++;
-x = 0;
-_putchar ('\n');
+/**
+* isPrime - analise if a number is prime or not
+* @n: number to check
+* Return: true if it is prime false if not
+*/
+int isPrime(long int n)
+{
+int i;
+
+if (n <= 1)
+{
+return (0);
 }
+else if (n == 2)
+{
+return (1);
 }
 else
-_putchar('\n');
+{
+for (i = 2; i < n; i++)
+{
+if (n % i == 0)
+{
+return (0);
+}
+}
+return (1);
+}
+}
+/**
+* biggestFactor - returns the biggest prime factor of a number
+* @a: number to check
+* Return: biggest factor
+*/
+long int biggestFactor(long int a)
+{
+long int i, factor;
+
+factor = a;
+for (i = 2; i <= factor; i++)
+{
+if (isPrime(factor) == 1)
+{
+break;
+}
+else
+{
+if ((factor % i == 0) && (isPrime(i) == 1))
+{
+factor = factor / i;
+continue;
+}
+else
+{
+factor = factor;
+}
+}
+}
+return (factor);
 }

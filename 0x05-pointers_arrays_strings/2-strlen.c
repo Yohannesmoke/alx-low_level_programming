55b7b411d7d474f554ace15c1d427f4d91a695c9
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-11 03:53:06 -0700

    the third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen -> return length of string
+* @s: parameter 1
+* Return: vale of the char
+*/
+int _strlen(char *s)
+{
+char str[100];
+for (s = 0; str[s] != '\0'; ++i)
+{
+_putchar('s');
+}
+}

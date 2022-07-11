e0db7dc31c0ec97469f923497bb436a4ac0b1b0f
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-11 04:27:09 -0700

    the go

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
 * _strlen -> return length of string
 * @s: parameter 1
@@ -7,7 +8,7 @@
 int _strlen(char *s)
 {
 int len;
-for (; *s != '\0'; s++)
+for (s = 0; *s != '\0'; s++)
 {
 len += 1;
 }

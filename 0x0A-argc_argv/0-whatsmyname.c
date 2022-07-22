cf6b3642cebd5c81ef6eac6c90b0e224cdc30ba4
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-22 10:11:52 -0700

    th

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main -> this fun for printing name
+* @argc: parameter
+* @arcv: parameter 2
+* Return: 0 for success
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+}

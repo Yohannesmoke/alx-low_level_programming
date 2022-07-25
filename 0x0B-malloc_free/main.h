1a71bec6a231e11c3a0e2b3858f351ff249f6cd5
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-07-25 06:34:34 -0700

    the

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
 
 
 #endif

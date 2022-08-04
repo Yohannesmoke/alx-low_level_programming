eed69dc5c623b899020fdd759f6986f9cfc3638e
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-08-04 09:53:15 -0700

    no 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
-#include <stdarg.c>
+#include "variadic_functions.h"
 
 /**
 * sum_them_all - function that returns the sum of all its parameters.
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);

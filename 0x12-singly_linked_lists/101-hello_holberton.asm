44281ec0f6449df4d86e3fe9bb927d947cbac315
Yohannesmoke <yohannesmekonen091355@gmail.com>
2022-08-11 08:27:14 -0700

    hot fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,13 @@
-;--------------------------------------------------------------------------------------
-;
- 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
-; Compiled using nasm and gcc.
-; nasm -f elf64 101-hello_holberton.asm && gcc 101-hello_holberton.o -o hello
-;
-;--------------------------------------------------------------------------------------
 
-section .data
-format db "%s", 10, 0
-message db "Hello, Holberton", 0
+global 	main
+	extern 	printf
 
-section .text
-extern printf ;C function to be called
-global main ;main function
-
+	section .text
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, message
-	mov rax, 0 ;can be xor rax, rax
-	call printf ;call C function.
-
-	pop rbp ;restor stack
-
-	mov rax, 0 ;normal, no error, return value
-	ret ;return
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
